87e5b7bd575acd01f015e043f7eab03cc11c59b0
zangtengfei <81709456@qq.com>
2021-07-27 18:11:39 +0800

    fix(usemqtt): 区分页面无活动下线和重复登录下线,给出不同提示

--- src/hooks/useMqtt/useMqtt.tsx
+++ src/hooks/useMqtt/useMqtt.tsx
@@ -1,7 +1,9 @@
 import { useState, useEffect, useCallback } from 'react';
-import mqtt, { MqttClient } from 'mqtt';
+import type { MqttClient } from 'mqtt';
+import mqtt from 'mqtt';
 import { message } from 'antd';
 import { history } from 'umi';
+import { useEventListener } from 'ahooks';
 
 interface Messages {
   topic: string;
@@ -13,8 +15,19 @@ const useMqtt = () => {
   const [messages, setMessages] = useState<Messages>();
   const [reconnectTimes, setReconnectTimes] = useState(0);
   const [connectStatus, setConnectStatus] = useState<string>('UnConnected');
+  const [visible, setVisible] = useState<boolean>(true);
+
+  useEventListener('visibilitychange', () => {
+    console.log(document.visibilityState);
+    if (document.visibilityState === 'visible') {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  });
 
   const setMqttStatus = useCallback((status: string) => {
+    console.log(status);
     setConnectStatus(status);
   }, []);
 
@@ -50,18 +63,12 @@ const useMqtt = () => {
         setMqttStatus('Closed');
       });
       client.on('reconnect', () => {
+        // if (visible) {
+        //   setReconnectTimes(reconnectTimes + 1);
+        // } else {
+        //   setReconnectTimes(0);
+        // }
         setReconnectTimes((times) => times + 1);
-        if (reconnectTimes > 3) {
-          if (client) {
-            client.end();
-          }
-
-          localStorage.clear();
-          history.replace({
-            pathname: '/login',
-          });
-          message.error('有人登陆您的账号，已被迫下线！');
-        }
       });
       client.on('disconnect', () => {
         setMqttStatus('Disconnected');
@@ -70,7 +77,31 @@ const useMqtt = () => {
         setMqttStatus('Offline');
       });
     }
-  }, [client, reconnectTimes]);
+  }, [visible, client, reconnectTimes, setMqttStatus]);
+
+  useEffect(() => {
+    if (reconnectTimes > 1) {
+      if (client) {
+        client.end();
+      }
+
+      localStorage.clear();
+      history.replace({
+        pathname: '/login',
+      });
+      if (visible) {
+        message.error('有人登陆您的账号，已被迫下线！', 5);
+      } else {
+        message.error({
+          content: '页面长时间处于无活动状态，已主动下线！',
+          duration: 0,
+          onClick: () => {
+            message.destroy();
+          },
+        });
+      }
+    }
+  }, [visible, client, reconnectTimes]);
 
   useEffect(() => {
     return () => {
